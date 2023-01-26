e2c2910d5be62dd091f11d1e368609ec43a0ab7c
AbdulRehman <likeam99@gmail.com>
2023-01-27 04:35:45 +0500

    Display info

--- src/App.js
+++ src/App.js
@@ -5,24 +5,45 @@ function App() {
   const [age, setAge] = useState(0);
   const [hight, setHight] = useState(0);
   const [superPower, setSuperPower] = useState("");
+  const [displayChrarcter, setDisplayCharcter] = useState(false);
 
   
 
 
   return (
     <div className="App">
-      <h1>welcom from App</h1>
-      <label >Nanme</label>
-      <input type="text" 
-      onChange={(event) =>{setName(event.target.value)}} />
-      <label htmlFor="">age</label>
-      <input type="number" />
-      <label htmlFor="">Hight</label>
-      <input type="number" />
-      <label htmlFor="">Power</label>
-      <input type="text" />
-
-      <button onClick={() => setName('Rehman')}>Change Name: {name}</button>
+      <h1>Build A Hero</h1>
+      <div>
+        <label >Nanme</label>
+        <input type="text" 
+        onChange={(event) =>{setName(event.target.value)}} />
+        <br />
+        <label htmlFor="">Age</label>
+        <input type="number" 
+        onChange={(event) =>{setAge(event.target.value)}} />
+        <br />
+        <label htmlFor="">Hight</label>
+        <input type="number" 
+        onChange={(event) =>{setHight(event.target.value)}} />
+        <br />
+        <label htmlFor="">Power</label>
+        <input type="text" onChange={(event) =>{setSuperPower(event.target.value)}} /> 
+      </div>
+      <br />
+      <br />
+      <button 
+      onClick={() => {setDisplayCharcter(true)}}>Display Character</button>
+      <div>
+        <h1>Hero Info</h1>
+        {displayChrarcter && (
+          <ul>
+            <li>{name}</li>
+            <li>{age}</li>
+            <li>{hight}</li>
+            <li>{superPower}</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
