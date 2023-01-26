1fa1cb8a69ce0cb4eaaf21884352f68a7890ee72
AbdulRehman <likeam99@gmail.com>
2023-01-26 19:48:55 +0500

    state using

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [hight, setHight] = useState(0);
+  const [superPower, setSuperPower] = useState("");
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>welcom from App</h1>
+      <label >Nanme</label>
+      <input type="text" 
+      onChange={(event) =>{setName(event.target.value)}} />
+      <label htmlFor="">age</label>
+      <input type="number" />
+      <label htmlFor="">Hight</label>
+      <input type="number" />
+      <label htmlFor="">Power</label>
+      <input type="text" />
+
+      <button onClick={() => setName('Rehman')}>Change Name: {name}</button>
     </div>
   );
 }
